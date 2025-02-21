7a5f1a70133ba624a6aeb5ade56fb51ef26ab1c4
Alexander199824 <mismaleje@miumg.edu.gt>
2025-02-20 23:47:43 -0600

    Add memory optimizations while maintaining stability

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Manejar todas las rutas enviando el archivo index.html
-app.get('/*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Obtener el puerto del entorno o usar 3000 como fallback
-const port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log(`Server is running on port ${port}`);
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log('Server running on port', port);
 });
